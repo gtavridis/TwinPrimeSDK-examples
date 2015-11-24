4e3d3ba89f70b514879b86e774e4421e750013cb
Wojciech A. Koszek <wkoszek@twinprime.com>
2015-11-23 19:17:10 -0800

    Don't attempt to code sign on Travis.

--- build.sh
+++ build.sh
@@ -7,12 +7,17 @@
 echo "# About to build all Twin Prime SDK sample iOS apps"
 echo "# For this to work you must have CocoaPods installed"
 
+OPTS=""
+if [ "x${TRAVIS}" != "x" ]; then
+	OPTS="CODE_SIGNING_REQUIRED=NO"
+fi
+
 set -x
 for APP in SampleApp SampleSwiftApp AFSample; do
 	echo "# building $APP"
 	(
 		cd $APP
 		pod install
-		xcodebuild -workspace ${APP}.xcworkspace -scheme $APP
+		xcodebuild $OPTS -workspace ${APP}.xcworkspace -scheme $APP
 	)
 done
